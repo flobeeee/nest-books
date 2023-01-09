e77ad878f5a85d4a7e1ded19d1760226ca7705d5
ejlee <ejlee@mediance.co.kr>
2023-01-09 16:01:48 +0900

    database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from './database/database.module';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { BookController } from './book/book.controller';
+import { BookService } from './book/book.service';
+import { bookProviders } from './book/book.providers';
+import { ConfigModule } from '@nestjs/config'; // env
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [DatabaseModule, ConfigModule.forRoot()],
+  controllers: [AppController, BookController],
+  providers: [BookService, ...bookProviders],
 })
 export class AppModule {}
