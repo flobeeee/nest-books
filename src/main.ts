d4f7d52873f23321db1d294dd7b42b8b99ca2536
ejlee <ejlee@mediance.co.kr>
2023-01-10 17:09:42 +0900

    pipe 에러 수정

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,12 @@ import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  app.useGlobalPipes
-  new ValidationPipe({
-    whitelist: true, // 인자로 받는 것만 들어오게 할 수 있음
-    forbidNonWhitelisted: true, // 메세지로 해당 인자는 사용할 수 없다고 안내함
-  }),
-    await app.listen(3000)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // 인자로 받는 것만 들어오게 할 수 있음
+      forbidNonWhitelisted: true, // 메세지로 해당 인자는 사용할 수 없다고 안내함
+    }),
+  )
+  await app.listen(3000)
 }
 bootstrap()
