96ee723f167ec56d5bb099f82746c21f041580dc
ejlee <ejlee@mediance.co.kr>
2023-01-11 15:21:36 +0900

    test

--- src/books/books.controller.spec.ts
+++ src/books/books.controller.spec.ts
@@ -1,24 +1,53 @@
-import { Repository } from 'typeorm'
+import { Test } from '@nestjs/testing'
 import { BooksController } from './books.controller'
-import { Books } from './books.entity'
 import { BooksService } from './books.service'
+import { PostBookDto } from './dto/post-book.dto'
 
 describe('CatsController', () => {
   let booksController: BooksController
-  let booksService: BooksService
-  let booksRepository: Repository<Books>
 
-  beforeEach(() => {
-    booksService = new BooksService(booksRepository)
-    booksController = new BooksController(booksService)
+  beforeEach(async () => {
+    const moduleRef = await Test.createTestingModule({
+      controllers: [BooksController],
+      providers: [
+        {
+          provide: BooksService,
+          useValue: {
+            create: jest
+              .fn()
+              .mockImplementation((book: PostBookDto) =>
+                Promise.resolve({ id: '1', ...book }),
+              ),
+            findAll: jest.fn().mockResolvedValue([
+              {
+                name: '나무',
+                genre: '소설',
+              },
+              {
+                name: '넷플릭스',
+                genre: '비즈니스',
+              },
+            ]),
+            findOne: jest.fn().mockImplementation((id: string) =>
+              Promise.resolve({
+                name: 'firstName #1',
+                genre: 'lastName #1',
+                id,
+              }),
+            ),
+            remove: jest.fn(),
+          },
+        },
+      ],
+    }).compile()
+
+    booksController = moduleRef.get<BooksController>(BooksController)
   })
 
-  describe('findAll', () => {
-    it('should return an array of books', async () => {
-      let result: Promise<Books[]>
-      jest.spyOn(booksService, 'findAll').mockImplementation(() => result)
-
-      expect(await booksController.cgetAction(null)).toBe(result)
+  describe('findAll()', () => {
+    it('should find all users ', async () => {
+      const data = await booksController.cgetAction(null)
+      console.log(data)
     })
   })
 })
